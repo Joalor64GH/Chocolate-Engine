6f5198d223207cedbe7feed69f31e7749ff0f4f6
Joalor64 <rhgarcon@gmail.com>
2022-09-29 16:40:39 -0400

    very low iq indeed

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -126,7 +126,7 @@ class CreditsState extends MusicBeatState
 	function changeSelection(change:Int = 0)
 	{
 
-		FlxG.sound.play('assets/sounds/scrollMenu' + TitleState.soundExt, 0.4);
+		FlxG.sound.play('assets/sounds/scrollMenu', 0.4);
 
 		curSelected += change;
 
