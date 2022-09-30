6d3bc56b77a10b3a7a70eff857d69c0c55026ffb
Joalor64 <rhgarcon@gmail.com>
2022-09-29 20:19:21 -0400

    credits code from mag engine

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -1,72 +1,136 @@
 package;
 
-import Section.SwagSection;
+#if desktop
+import Discord.DiscordClient;
+#end
 import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.system.FlxSound;
 import flixel.math.FlxMath;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.addons.text.FlxTypeText;
 import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import lime.utils.Assets;
-import lime.system.System;
 #if sys
-import flixel.system.FlxSound;
+import sys.io.File;
+import sys.FileSystem;
 #end
+
 using StringTools;
 
-class CreditsState extends MusicBeatState
+class CreditsMenu extends MusicBeatState
 {
-	public static var currentSongList:Array<String> = [];
-	public static var soundTest:Bool = false;
-	var vocals:FlxSound;
-	var songs:Array<String> = [];
+	var credits:Array<CreditsMetadata> = [];
 
-	var selector:FlxText;
-	var curSelected:Int = 0;
-	var usingCategoryScreen:Bool = false;
-	private var grpSongs:FlxTypedGroup<Alphabet>;
-	private var curPlaying:Bool = false;
+	static var curSelected:Int = 0;
+
+	private var grpCredits:FlxTypedGroup<Alphabet>;
+
+	var descText:FlxText;
+	var bg:FlxSprite;
+	var colorTween:FlxTween;
 
 	override function create()
 	{
-		songs = CoolUtil.coolTextFile('assets/data/credits.txt');
+		MemoryManager.freeTrashedAssets();
+		MemoryManager.freeAllAssets();
 
-		var isDebug:Bool = false;
+		if (FlxG.save.data.mousescroll)
+		{
+			FlxG.mouse.visible = true;
+		}
 
-		#if debug
-		isDebug = true;
+		var initCreditlist = CoolUtil.coolTextFile(Paths.txt('data/creditsList'));
+
+		if (FileSystem.exists(Paths.modTxt('data/creditsList')) && FileSystem.exists(Paths.txt('data/creditsList')))
+		{
+			initCreditlist = File.getContent(Paths.modTxt('data/creditsList')).trim().split('\n');
+
+			for (i in 0...initCreditlist.length)
+			{
+				initCreditlist[i] = initCreditlist[i].trim();
+			}
+		}
+		else
+		{
+			initCreditlist = CoolUtil.coolTextFile(Paths.txt('data/creditsList'));
+		}
+
+		for (i in 0...initCreditlist.length)
+		{
+			var data:Array<String> = initCreditlist[i].split(':');
+			credits.push(new CreditsMetadata(data[0], data[1]));
+		}
+
+		/* 
+			if (FlxG.sound.music != null)
+			{
+				if (!FlxG.sound.music.playing)
+					FlxG.sound.playMusic(Paths.music('freakyMenu'));
+			}
+		 */
+
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
 		#end
 
-		// LOAD MUSIC
-
-		// LOAD CHARACTERS
-		var bg:FlxSprite = new FlxSprite().loadGraphic('assets/images/menuBGBlue.png');
+		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = FlxColor.PINK;
 		add(bg);
 
-		grpSongs = new FlxTypedGroup<Alphabet>();
-		add(grpSongs);
+		descText = new FlxText(50, 600, 1180, "", 32);
+		descText.setFormat(Paths.font("funkin.otf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		descText.scrollFactor.set();
+		descText.text = 'what';
+		descText.borderSize = 2.4;
+		add(descText);
 
-		for (i in 0...songs.length)
+		grpCredits = new FlxTypedGroup<Alphabet>();
+		add(grpCredits);
+
+		for (i in 0...credits.length)
 		{
-			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, songs[i], true, false);
-			songText.isMenuItem = true;
-			songText.targetY = i;
-			grpSongs.add(songText);
-			// songText.x += 40;
+			var creditText:Alphabet = new Alphabet(0, (70 * i) + 30, credits[i].modderName, true, false);
+			creditText.isMenuItem = true;
+			creditText.targetY = i;
+			grpCredits.add(creditText);
+
+			// creditText.x += 40;
 			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
-			// songText.screenCenter(X);
+			// creditText.screenCenter(X);
 		}
 
 		changeSelection();
-
-		// FlxG.sound.playMusic('assets/music/title' + TitleState.soundExt, 0);
+		// FlxG.sound.playMusic(Paths.music('title'), 0);
 		// FlxG.sound.music.fadeIn(2, 0, 0.8);
-		// add(selector);
 
-		var swag:Alphabet = new Alphabet(1, 0, "swag");
+		var descText:FlxText = new FlxText(50, 600, 1180, "", 32);
+		descText.setFormat(Paths.font("funkin.otf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		descText.scrollFactor.set();
+		descText.borderSize = 2.4;
+		add(descText);
+
+		// JUST DOIN THIS SHIT FOR TESTING!!!
+		/* 
+			var md:String = Markdown.markdownToHtml(Assets.getText('CHANGELOG.md'));
+
+			var texFel:TextField = new TextField();
+			texFel.width = FlxG.width;
+			texFel.height = FlxG.height;
+			// texFel.
+			texFel.htmlText = md;
+
+			FlxG.stage.addChild(texFel);
+
+			// scoreText.textField.htmlText = md;
+
+			trace(md);
+		 */
 
 		super.create();
 	}
@@ -75,46 +139,43 @@ class CreditsState extends MusicBeatState
 	{
 		super.update(elapsed);
 
-
 		var upP = controls.UP_P;
 		var downP = controls.DOWN_P;
 		var accepted = controls.ACCEPT;
+		var space = FlxG.keys.justPressed.SPACE;
+
+		var shiftMult:Int = 1;
+		if (FlxG.keys.pressed.SHIFT)
+			shiftMult = 3;
+
 		if (upP)
 		{
-			changeSelection(-1);
+			FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 		}
 		if (downP)
 		{
-			changeSelection(1);
+			FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 		}
-		
 
 		if (controls.BACK)
-		{
-			// main menu or else we are cursed
-			LoadingState.loadAndSwitchState(new MainMenuState());
-				
-		}
-
+			MusicBeatState.switchState(new MainMenuState());
 	}
 
 	function changeSelection(change:Int = 0)
 	{
-
-		FlxG.sound.play('assets/sounds/scrollMenu', 0.4);
-
 		curSelected += change;
 
 		if (curSelected < 0)
-			curSelected = songs.length - 1;
-		if (curSelected >= songs.length)
+			curSelected = credits.length - 1;
+		if (curSelected >= credits.length)
 			curSelected = 0;
 
-		// selector.y = (70 * curSelected) + 30;
+		descText.text = credits[curSelected].desc;
 
+		// selector.y = (70 * curSelected) + 30;
 		var bullShit:Int = 0;
 
-		for (item in grpSongs.members)
+		for (item in grpCredits.members)
 		{
 			item.targetY = bullShit - curSelected;
 			bullShit++;
@@ -128,5 +189,17 @@ class CreditsState extends MusicBeatState
 				// item.setGraphicSize(Std.int(item.width));
 			}
 		}
+	}
+}
+
+class CreditsMetadata
+{
+	public var modderName:String = "";
+	public var desc:String = "";
+
+	public function new(name:String, desc:String)
+	{
+		this.modderName = name;
+		this.desc = desc;
 	}
 }
