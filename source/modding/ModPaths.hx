eaa10d700a75fa7c6405fcaa4cee3077603e443f
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-11 21:04:59 -0400

    bitch I used privateAccess why the fuck aren't you working?

--- source/modding/ModPaths.hx
+++ source/modding/ModPaths.hx
@@ -6,7 +6,9 @@ import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenflAssets;
 
 class ModPaths {
-    @:privateAccess static final currentLevel:String = Paths.currentLevel;
+    @:privateAccess {
+        static final currentLevel:String = Paths.currentLevel;
+    }
 
     private static final SOUND_EXT = Paths.SOUND_EXT;
 
