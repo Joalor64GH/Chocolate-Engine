4c18562edc08a1927533822dceb4ed70f5942242
Joalor64 <rhgarcon@gmail.com>
2022-10-16 18:48:11 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -110,7 +110,7 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		addChild(new DateTime(10, 3, '0xFFFFFF');
+		addChild(new DateTime(10, 3, '0xFFFFFF'));
 
 		addChild(new FPS(10, 3, 0xFFFFFF));
 		
