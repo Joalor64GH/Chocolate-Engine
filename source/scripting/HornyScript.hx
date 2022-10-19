8d16b85a245e63b591b96d6bff4a9a9152023a13
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-19 17:36:32 -0400

    an experiment cuz I got bored

--- source/scripting/HornyScript.hx
+++ source/scripting/HornyScript.hx
@@ -2,27 +2,29 @@ package scripting;
 
 // it keeps crashing the game :shrug:
 // maybe because there's literally nothing here
+import flixel.FlxG;
 import PlayState;
 
-class HornyScript {
+class HornyScript extends PlayState {
     public function onCreate() {
-
+        super.onCreate();
     }
 
-    public function onUpdate() {
-
+    public function onUpdate(elasped:Float) {
+        elapsed = FlxG.elapsed;
+        super.onUpdate(elasped);
     }
 
     public function onNoteHit() {
-
+        super.onNoteHit();
     }
 
     public function onNoteMiss() {
-
+        super.onNoteMiss();
     }
 
     public function onPlayerDeath() {
-
+        super.onPlayerDeath();
     }
 
 }
