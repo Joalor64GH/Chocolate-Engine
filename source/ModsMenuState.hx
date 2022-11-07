04cfeb09e23736b631111489ce4d9e9eeea52242
Joalor64 <rhgarcon@gmail.com>
2022-11-07 17:52:31 -0500

    i almost forgot

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -1,7 +1,7 @@
 /*
  * Apache License, Version 2.0
  *
- * Copyright (c) 2022 MemeHoovy, Joalor64, and Wither362
+ * Copyright (c) 2022 MemeHoovy, Joalor64 and Wither362
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
