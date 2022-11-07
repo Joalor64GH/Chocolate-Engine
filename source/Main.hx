c775fe464a9cea2c662565b3faf631435e6dcadd
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-11-07 15:42:02 -0500

    kms

--- source/Main.hx
+++ source/Main.hx
@@ -23,6 +23,8 @@ import sys.io.File;
 import sys.io.Process;
 #end
 
+using StringTools;
+
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
