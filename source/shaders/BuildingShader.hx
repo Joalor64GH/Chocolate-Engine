304ccb9bc887f04edd67d09bb715f7ade43eb586
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-11-28 19:05:00 -0500

    subclasses exist :|

--- source/shaders/BuildingShader.hx
+++ source/shaders/BuildingShader.hx
@@ -115,3 +115,23 @@ class BuildingShader extends FlxGraphicsShader
 		super();
 	}
 }
+
+class BuildingShaders
+{
+	public var shader(default, null):BuildingShader = new BuildingShader();
+
+	public function new():Void
+	{
+		shader.alphaShit.value = [0];
+	}
+
+	public function update(elapsed:Float):Void
+	{
+		shader.alphaShit.value[0] += elapsed;
+	}
+
+	public function reset():Void
+	{
+		shader.alphaShit.value[0] = 0;
+	}
+}
