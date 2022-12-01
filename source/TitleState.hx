e1d40253f16e9021d42d14d3a4545d4d4ab4f0bf
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-01 16:24:14 +0100

    It’s imported so…
    Should we add a flashing lights option?

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -90,25 +90,25 @@ class TitleState extends MusicBeatState
 	override public function create():Void
 	{
 		#if MODS_ALLOWED
-		if (sys.FileSystem.exists('mods/'))
+		if (FileSystem.exists('mods/'))
 		{
 			var folders:Array<String> = [];
-			for (file in sys.FileSystem.readDirectory('mods/'))
+			for (file in FileSystem.readDirectory('mods/'))
 			{
 				var path = haxe.io.Path.join(['mods/', file]);
-				if (sys.FileSystem.isDirectory(path))
+				if (FileSystem.isDirectory(path))
 				{
 					folders.push(file);
 				}
 			}
 		}
-		if (sys.FileSystem.exists('mods/' + ModsMenuState.coolId + '/'))
+		if (FileSystem.exists('mods/' + ModsMenuState.coolId + '/'))
 		{
 			var folders:Array<String> = [];
-			for (file in sys.FileSystem.readDirectory('mods/' + ModsMenuState.coolId + '/'))
+			for (file in FileSystem.readDirectory('mods/' + ModsMenuState.coolId + '/'))
 			{
 				var path = haxe.io.Path.join(['mods/' + ModsMenuState.coolId + '/', file]);
-				if (sys.FileSystem.isDirectory(path))
+				if (FileSystem.isDirectory(path))
 				{
 					folders.push(file);
 				}
